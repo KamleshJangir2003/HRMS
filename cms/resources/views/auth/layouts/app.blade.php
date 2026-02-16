f41e08543b963150f3aadaebe872f7121ca9a1d2
kamleshjangir2003 <rahulprajapat9277>
2026-02-16 10:09:39 +0530

    update

--- cms/resources/views/auth/layouts/app.blade.php
+++ cms/resources/views/auth/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Kwikster CMS</title>
+    <title>Kwikster HRMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
